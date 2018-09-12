de01f5e0184dd1beb783ee0bc3017932b9e1e666
Elson Roque Caldas Dominguez <roque.unt@gmail.com>
2018-09-12 01:08:38 -0500

    cambios en la pantalla desde el view model

--- admision/admision/vistaModelos/LoginVistaModelo.cs
+++ admision/admision/vistaModelos/LoginVistaModelo.cs
@@ -5,23 +5,50 @@ using System.Text;
 namespace admision.vistaModelos
 {
     using GalaSoft.MvvmLight.Command;
+    using System.ComponentModel;
     using System.Windows.Input;
     using Xamarin.Forms;
 
-    class LoginVistaModelo
+    class LoginVistaModelo : BaseVistaModelo
     {
-        public String txtUsuario { get; set; }
-        public String txtClave { get; set; }
-        public bool indicador { get; set; }
-        
+
+
+        #region atributos
+        private string txtClaveCambiar;
+        private bool indicadorCambiar;
+        #endregion
+
+        #region propiedades 
+        public String txtUsuario
+        {
+            get;
+            set;
+        }
+        public String txtClave
+        {
+            get { return this.txtClaveCambiar; }
+            set { SetValue(ref this.txtClaveCambiar,value); }
+        }
+        public bool indicador
+        {
+            get { return this.indicadorCambiar; }
+            set { SetValue(ref this.indicadorCambiar, value); }
+        }
+        #endregion
+
+        #region constructor
         public LoginVistaModelo()
         {
+            this.txtUsuario = string.Empty;
+            this.txtClave = string.Empty;
             this.indicador = false;
         }
+        #endregion
 
 
         #region comandos
-        public ICommand aceptarCommand {
+        public ICommand aceptarCommand
+        {
             get
             {
                 return new RelayCommand(aceptar);
@@ -30,19 +57,19 @@ namespace admision.vistaModelos
         }
         private async void aceptar()
         {
+            this.indicador = true;
             if (string.IsNullOrEmpty(this.txtUsuario) || string.IsNullOrEmpty(this.txtClave))
             {
                 await Application.Current.MainPage.DisplayAlert(
-                    "Mensaje",
-                    "Usuario y Clave son obligatorios",
+                    "Error",
+                    "los campos usuario y clave son obligatorios",
                     "Aceptar");
                 return;
             }
 
         }
-
         #endregion
 
     }
-    
+
 }
