7aae5200c77b19f4d6a22d6f5c03f89a5622edf8
Elson Roque Caldas Dominguez <roque.unt@gmail.com>
2018-09-11 23:32:14 -0500

    mensaje de error por campos vacios en el login

--- admision/admision/vistaModelos/LoginVistaModelo.cs
+++ admision/admision/vistaModelos/LoginVistaModelo.cs
@@ -4,19 +4,45 @@ using System.Text;
 
 namespace admision.vistaModelos
 {
+    using GalaSoft.MvvmLight.Command;
     using System.Windows.Input;
+    using Xamarin.Forms;
+
     class LoginVistaModelo
     {
         public String txtUsuario { get; set; }
         public String txtClave { get; set; }
         public bool indicador { get; set; }
-        public ICommand btnAceptar { get; set; }
+        
         public LoginVistaModelo()
         {
             this.indicador = false;
         }
 
-        
+
+        #region comandos
+        public ICommand aceptarCommand {
+            get
+            {
+                return new RelayCommand(aceptar);
+            }
+
+        }
+        private async void aceptar()
+        {
+            if (string.IsNullOrEmpty(this.txtUsuario) || string.IsNullOrEmpty(this.txtClave))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Mensaje",
+                    "Usuario y Clave son obligatorios",
+                    "Aceptar");
+                return;
+            }
+
+        }
+
+        #endregion
 
     }
+    
 }
